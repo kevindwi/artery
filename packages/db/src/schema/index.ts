354ab0f737e37504c29e2497db8b446a6146ccf1
kevindwi <kevindwi356@gmail.com>
2025-12-24 23:25:04 +0700

    Refactor: Move user schema to its own file

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,3 +1,11 @@
 export * from "./auth";
 export * from "./todo";
+export * from "./user";
+export * from "./organization";
+export * from "./template";
+export * from "./datastream";
+export * from "./device";
+export * from "./deviceState";
+export * from "./telemetry";
+
 export {};
