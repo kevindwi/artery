092646acb6a0561a8f562396df9a7815ea64ffec
kevindwi <kevindwi356@gmail.com>
2025-12-25 00:07:35 +0700

    Export drizzle-orm types and functions

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
+export * from "drizzle-orm";
+
 import * as schema from "./schema";
 
 export const db = drizzle(process.env.DATABASE_URL || "", { schema });
