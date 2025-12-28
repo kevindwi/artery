6f8e271d6b8a43d4c4b81a425245a7ddf0ebaa65
kevindwi <kevindwi356@gmail.com>
2025-12-28 22:00:13 +0700

    Add template router to app router

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,5 +1,6 @@
 import { protectedProcedure, publicProcedure, router } from "../index";
 import { organizationRouter } from "./organization";
+import { templateRouter } from "./template";
 import { todoRouter } from "./todo";
 
 export const appRouter = router({
@@ -14,5 +15,6 @@ export const appRouter = router({
   }),
   todo: todoRouter,
   organization: organizationRouter,
+  template: templateRouter,
 });
 export type AppRouter = typeof appRouter;
