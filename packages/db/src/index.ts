64bc9be8983e3919b03dac5581ac3b359f7929ae
kevindwi <kevindwi356@gmail.com>
2026-01-03 12:06:04 +0700

    Add DbClient type alias

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,12 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import type { Pool } from "pg";
 
 export * from "drizzle-orm";
 
 import * as schema from "./schema";
+
+export type DbClient = NodePgDatabase<typeof schema> & {
+  $client: Pool;
+};
 
 export const db = drizzle(process.env.DATABASE_URL || "", { schema });
