bfe4684c7068c6177994d41d8c0d75e686e7dd53
kevindwi <kevindwi356@gmail.com>
2026-01-08 18:44:28 +0700

    Add device CRUD and status update functionality
    This commit introduces the ability to create, read, update, and delete devices via
    the API. It also adds functionality to update a device's status and regenerate its
    authentication token. The necessary dependencies and schema changes are included to
    support these features.

--- packages/api/src/routers/device.ts
+++ packages/api/src/routers/device.ts
@@ -1,16 +1,60 @@
-import z from "zod";
-import { organizationProcedure, router } from "../index";
-import { deviceService } from "../services/device";
+import { z } from "zod";
+import {
+  canCreateDevice,
+  canDeleteDevice,
+  canUpdateDevice,
+  deviceProcedure,
+  organizationProcedure,
+  router,
+} from "../index";
+import {
+  createDeviceSchema,
+  deviceService,
+  updateDeviceSchema,
+} from "../services/device";
 
 export const deviceRouter = router({
-  all: organizationProcedure
-    .input(z.object({ organizationId: z.string() }))
-    .query(async ({ ctx }) => {
-      return await deviceService.getAll(ctx.activeOrgId);
-    }),
+  all: organizationProcedure.query(async ({ ctx }) => {
+    return await deviceService.getAll(ctx.activeOrgId);
+  }),
   byId: organizationProcedure
-    .input(z.object({ deviceId: z.string() }))
+    .input(
+      z.object({
+        deviceId: z.string().min(12),
+      }),
+    )
     .query(async ({ ctx, input }) => {
-      return await deviceService.getById(ctx.activeOrgId, input.deviceId);
+      const { deviceId } = input;
+      return await deviceService.getById(ctx.activeOrgId, deviceId);
+    }),
+  create: canCreateDevice
+    .input(createDeviceSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx.session.session;
+
+      return await deviceService.create(userId, {
+        ...input,
+        organizationId: ctx.activeOrgId,
+      });
+    }),
+  update: canUpdateDevice
+    .input(updateDeviceSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await deviceService.update(ctx.activeOrgId, input);
+    }),
+  delete: canDeleteDevice
+    .input(z.object({ id: z.string().min(12) }))
+    .mutation(async ({ ctx, input }) => {
+      return await deviceService.delete(ctx.activeOrgId, input.id);
+    }),
+  regenerateToken: canUpdateDevice
+    .input(z.object({ id: z.string().min(12) }))
+    .mutation(async ({ ctx, input }) => {
+      return await deviceService.regenerateToken(ctx.activeOrgId, input.id);
+    }),
+  updateStatus: deviceProcedure
+    .input(z.object({ id: z.string().min(12), status: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await deviceService.updateStatus(ctx.device.id, input.status);
     }),
 });
--- packages/api/src/services/device.ts
+++ packages/api/src/services/device.ts
@@ -1,6 +1,23 @@
 import { and, db, eq } from "@artery/db";
+import { z } from "zod";
 import { device } from "@artery/db/schema/device";
 import { TRPCError } from "@trpc/server";
+import { init } from "@paralleldrive/cuid2";
+
+export const createDeviceSchema = z.object({
+  name: z.string().min(1),
+  templateId: z.string(),
+});
+
+export const updateDeviceSchema = createDeviceSchema.extend({
+  id: z.string(),
+});
+
+const createId = init({
+  random: Math.random,
+  length: 32,
+  fingerprint: "e328d6c815d11bbe1bff637a20196505",
+});
 
 export const deviceService = {
   getAll: async (organizationId: string) => {
@@ -21,5 +38,85 @@ export const deviceService = {
     }
 
     return deviceDetail;
+  },
+  create: async (
+    userId: string,
+    data: z.infer<typeof createDeviceSchema> & {
+      organizationId: string;
+    },
+  ) => {
+    const authToken = createId();
+
+    const [newDevice] = await db
+      .insert(device)
+      .values({ ...data, createdBy: userId, authToken })
+      .returning();
+    return newDevice;
+  },
+  update: async (activeOrgId: string, data: z.infer<typeof updateDeviceSchema>) => {
+    const dv = await db.query.datastream.findFirst({
+      where: eq(device.id, data.id),
+    });
+
+    if (!dv) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Device not found",
+      });
+    }
+
+    const [updatedDevice] = await db
+      .update(device)
+      .set({ ...data })
+      .where(and(eq(device.organizationId, activeOrgId), eq(device.id, data.id)))
+      .returning();
+    return updatedDevice;
+  },
+  delete: async (activeOrgId: string, deviceId: string) => {
+    const ds = await db.query.device.findFirst({
+      where: eq(device.id, deviceId),
+    });
+
+    if (!ds) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Device not found",
+      });
+    }
+
+    const [deletedItem] = await db
+      .delete(device)
+      .where(and(eq(device.organizationId, activeOrgId), eq(device.id, deviceId)))
+      .returning();
+    return deletedItem;
+  },
+  regenerateToken: async (activeOrgId: string, deviceId: string) => {
+    const ds = await db.query.device.findFirst({
+      where: eq(device.id, deviceId),
+    });
+
+    if (!ds) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Device not found",
+      });
+    }
+
+    const authToken = createId();
+
+    const [updatedDevice] = await db
+      .update(device)
+      .set({ authToken })
+      .where(and(eq(device.organizationId, activeOrgId), eq(device.id, deviceId)))
+      .returning();
+    return updatedDevice;
+  },
+  updateStatus: async (deviceId: string, status: "ONLINE" | "OFFLINE") => {
+    const [updatedDevice] = await db
+      .update(device)
+      .set({ status })
+      .where(eq(device.id, deviceId))
+      .returning();
+    return updatedDevice;
   },
 };
